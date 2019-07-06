c6bbc489620cf875c4282f39e9c7941fa22bc695
Jorkoh <kohru95@gmail.com>
2019-07-06 15:12:03 +0200

    Added filtering by subreddit and comment shortening

--- Scripts/user_subscriber_bot.py
+++ Scripts/user_subscriber_bot.py
@@ -3,26 +3,45 @@ from datetime import datetime, timedelta
 
 from Scripts import login
 
-#TODO: Include posts
-#TODO: Create other acc to get notifications
-#TODO: Filter by subreddit
-#TODO: Test on Heroku
+# TODO: Include posts
+# TODO: Create other acc to get notifications
+# TODO: Test on Heroku
+
+activity_age_days = int(os.environ["activity_age_days"])
+subscribed_redditors = os.environ["subscribed_redditors"].split(", ")
+subscribed_subreddits = os.environ["subscribed_subreddits"].split(", ")
+
 
 def get_minimum_utc():
-    return (datetime.now() - timedelta(days=int(os.environ["activity_age_days"]))).timestamp()
+    return (datetime.now() - timedelta(days=activity_age_days)).timestamp()
 
 
 def get_redditors():
-    return [reddit.redditor(username) for username in os.environ["subscribed_redditors"].split(", ")]
+    return [reddit.redditor(username) for username in subscribed_redditors]
+
+
+def comment_passes_filters(comment):
+    return comment.subreddit.display_name in subscribed_subreddits
 
 
 def get_comments_from_redditor(redditor, minimum_utc):
     redditor_comments = []
     for comment in [comment for comment in redditor.comments.new(limit=None)]:
+        # Messages are ordered by post datetime from most recent, when minimum is reached ignore the rest
         if comment.created_utc < minimum_utc:
             break
-        redditor_comments.append(comment)
+        if comment_passes_filters(comment):
+            redditor_comments.append(comment)
     return redditor_comments
+
+
+def shorten_comment_permalink(permalink):
+    # Some elements of the permalink can be safely removed to keep the DM below the 10k character limit
+    elements = permalink.split('/')
+    del elements[0]
+    del elements[-1]
+    elements[-2] = ""
+    return '/'.join(elements)
 
 
 def compose_message(redditors_and_comments):
@@ -31,12 +50,12 @@ def compose_message(redditors_and_comments):
         redditor = redditor_and_comments[0]
         comments = redditor_and_comments[1]
         if len(comments) > 0:
-            message += f"**{redditor}** activity the last {os.environ['activity_age_days']} day(s):"
+            message += f"**{redditor}** activity:"
             for comment in comments:
-                message += f"\n\n* {comment.permalink}"
+                message += f"\n\n* {shorten_comment_permalink(comment.permalink)}"
             message += "\n\n"
         else:
-            message += f"**{redditor}** has not posted the last {os.environ['activity_age_days']} day(s).\n\n"
+            message += f"**{redditor}** has not posted.\n\n"
     return message
 
 
@@ -46,7 +65,7 @@ def run_script():
     for redditor in get_redditors():
         redditors_and_comments.append((redditor, get_comments_from_redditor(redditor, minimum_utc)))
     message = compose_message(redditors_and_comments)
-    reddit.user.me().message("This is a test", message)
+    reddit.user.me().message(f"Subscribed redditors activity the last {activity_age_days} day(s)", message)
 
 
 if __name__ == "__main__":
